5fdb37ff98b6ccd12eb869135237db2a64aa0cde
rbnis <5955614+rbnis@users.noreply.github.com>
2023-10-19 22:21:19 +0200

    "repair" setup env and add sdkconfig

--- setup-env.sh
+++ setup-env.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-export IDF_PATH="$(pwd)/esp-idf"
-
 virtualenv --python 3 --system-site-packages .venv
 source .venv/bin/activate
 
-# prepare esp-idf
-"${IDF_PATH}/tools/idf_tools.py" --non-interactive install
-"${IDF_PATH}/tools/idf_tools.py" --non-interactive install-python-env
+cd esp-idf
+./install.sh
+source export.sh
+cd ..
 
-. "${IDF_PATH}/export.sh"
