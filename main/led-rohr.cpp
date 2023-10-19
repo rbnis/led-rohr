a8b8ef0309b24f3c708d0c552f5b5a6323705710
Philipp Mandler <info@philipp-mandler.com>
2023-10-20 01:51:14 +0200

    Display something on the OLED

--- main/led-rohr.cpp
+++ main/led-rohr.cpp
@@ -1,8 +1,20 @@
 #include "Arduino.h"
 #include <Adafruit_NeoPixel/Adafruit_NeoPixel.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 const int PIN = 14;
 #define NUMPIXELS 10
+
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 extern "C" void app_main() {
@@ -17,4 +29,11 @@ extern "C" void app_main() {
     pixels.setPixelColor(i, pixels.Color(255, 0, 0, 255));
   }
 	pixels.show();
+
+	display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+	display.clearDisplay();
+	display.display();
+
+  display.drawPixel(10, 10, SSD1306_WHITE);
+	display.display();
 }
